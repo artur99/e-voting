2cc2da3453692de373095594f80eb9dcfc8e9739
artur99 <david1989mail@gmail.com>
2017-01-18 11:23:47 +0200

    Edituri chestii :D

--- app/app.php
+++ app/app.php
@@ -1,4 +1,13 @@
 <?php
 
-
 $app = new Silex\Application();
+
+$app->register(new Silex\Provider\ServiceControllerServiceProvider());
+$app->register(new Silex\Provider\DoctrineServiceProvider());
+$app->register(new Silex\Provider\SessionServiceProvider());
+$app->register(new Silex\Provider\TwigServiceProvider());
+
+
+include 'apploader.php';
+
+$app->run();
---
+++ app/apploader.php
@@ -0,0 +1,13 @@
+<?php
+
+// include 'misc.php';
+// include 'app/executers.php';
+include 'conf.php';
+// include 'langconf.php';
+
+include 'routers.php';
+
+include 'user/user_main.php';
+
+include 'models/model.php';
+include 'routers/routers.php';
